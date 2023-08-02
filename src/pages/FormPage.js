cf3ad8dbfd0dce65ade55979ef1d5626de954af7
kdansari01 <khusmuddin.ansari@gmail.com>
2023-08-02 14:07:23 +0530

    upd

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Swal from "sweetalert2";
 import { formFields } from "./formsField";
+import { useSelector } from "react-redux";
 
 const ApplicationFormPage = () => {
+  const getEmail = useSelector((state) => state.user.user);
   const { title } = useParams();
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: "",
-    email: "",
+    email: getEmail?.email || "",
     experience: "Less than a year",
     message: "",
     cv: null,
@@ -43,72 +45,82 @@ const ApplicationFormPage = () => {
   };
 
   return (
-    <div className="container mb-4 ">
-      <div className="row mx-0 justify-content-center">
-        <div className="col-md-8 col-lg-10 col-12 ">
-          <form
-            onSubmit={handleSubmit}
-            className="w-100 rounded-1 p-4 border bg-white shadow"
-            action="https://herotofu.com/start"
-            encType="multipart/form-data"
-          >
-            {formFields.map((field, index) => (
-              <div key={index} className="form-group mb-4">
-                <label className="d-block mb-1 form-label">{field.label}</label>
-                {field.type === "select" ? (
-                  <select
-                    name={field.name}
-                    value={formData[field.name]}
-                    onChange={handleChange}
-                    className="form-select"
-                    required={field.required}
-                  >
-                    {field.options.map((option, index) => (
-                      <option key={index} value={option}>
-                        {option}
-                      </option>
-                    ))}
-                  </select>
-                ) : field.type === "textarea" ? (
-                  <textarea
-                    name={field.name}
-                    value={formData[field.name]}
-                    onChange={handleChange}
-                    className="form-control"
-                    rows="3"
-                    placeholder={field.placeholder}
-                  />
-                ) : field.type === "file" ? (
-                  <input
-                    type="file"
-                    name={field.name}
-                    onChange={handleChange}
-                    className="form-control"
-                    required={field.required}
-                  />
-                ) : (
-                  <input
-                    type={field.type}
-                    name={field.name}
-                    value={formData[field.name]}
-                    onChange={handleChange}
-                    className="form-control"
-                    placeholder={field.placeholder}
-                    required={field.required}
-                  />
-                )}
-              </div>
-            ))}
+    <>
+      {getEmail?.email ? (
+        <div className="container mb-4 ">
+          <div className="row mx-0 justify-content-center">
+            <div className="col-md-8 col-lg-10 col-12 ">
+              <form
+                onSubmit={handleSubmit}
+                className="w-100 rounded-1 p-4 border bg-white shadow"
+                encType="multipart/form-data"
+              >
+                {formFields.map((field, index) => (
+                  <div key={index} className="form-group mb-4">
+                    <label className="d-block mb-1 form-label">
+                      {field.label}
+                    </label>
+                    {field.type === "select" ? (
+                      <select
+                        name={field.name}
+                        value={formData[field.name]}
+                        onChange={handleChange}
+                        className="form-select"
+                        required={field.required}
+                      >
+                        {field.options.map((option, index) => (
+                          <option key={index} value={option}>
+                            {option}
+                          </option>
+                        ))}
+                      </select>
+                    ) : field.type === "textarea" ? (
+                      <textarea
+                        name={field.name}
+                        value={formData[field.name]}
+                        onChange={handleChange}
+                        className="form-control"
+                        rows="3"
+                        placeholder={field.placeholder}
+                      />
+                    ) : field.type === "file" ? (
+                      <input
+                        type="file"
+                        name={field.name}
+                        onChange={handleChange}
+                        className="form-control"
+                        required={field.required}
+                      />
+                    ) : (
+                      <input
+                        type={field.type}
+                        name={field.name}
+                        value={formData[field.name]}
+                        onChange={handleChange}
+                        className="form-control"
+                        placeholder={field.placeholder}
+                        required={field.required}
+                      />
+                    )}
+                  </div>
+                ))}
 
-            <div className="mb-4 d-flex justify-content-end">
-              <button type="submit" className="btn btn-primary px-3 rounded-3">
-                Apply
-              </button>
+                <div className="mb-4 d-flex justify-content-end">
+                  <button
+                    type="submit"
+                    className="btn btn-primary px-3 rounded-3"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        navigate("/")
+      )}
+    </>
   );
 };
 
